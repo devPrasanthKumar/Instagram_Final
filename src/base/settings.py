ccc68e36455e574b5adc2ba42e58ae085dc66ff7
devPrasanthKumar <prok52010get@gmail.com>
2024-01-29 08:12:17 +0530

    Final Version Added

--- src/base/settings.py
+++ src/base/settings.py
@@ -88,12 +88,12 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # DATABASES = {
 #     "default": {
@@ -106,16 +106,16 @@ REST_FRAMEWORK = {
 #     }
 # }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "Instagram",
-        "USER": "kurinji",
-        "PASSWORD": "root",
-        # "HOST": "db",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "Instagram",
+#         "USER": "kurinji",
+#         "PASSWORD": "root",
+#         # "HOST": "db",
+#         "PORT": "5432",
+#     }
+# }
 
 JWT_AUTH = {
     # how long the original token is valid for
